b1ba4c04ea545f45ffac0ac9ce6065810ca57b97
Wilberth <wilberthlp4@gmail.com>
2021-09-18 18:02:03 -0500

    Create main app

---
+++ app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Request
+from typing import List
+from fastapi.params import Depends
+from starlette.responses import RedirectResponse
+from . import models, schemas
+from .Conexion import SessionLocal, engine
+from sqlalchemy.orm import Session
+from fastapi.templating import Jinja2Templates
+
+import json
+from fastapi.encoders import jsonable_encoder
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+templates = Jinja2Templates(directory='./templates')
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+@app.get('/', response_model=List[schemas.Lenguaje])
+def lenguajes(request:Request, db:Session=Depends(get_db)):
+    languages = db.query(models.Lenguaje).all()
+    result = jsonable_encoder(languages)
+    return templates.TemplateResponse('index.html', {'request': request, 'lista': result})
+
+@app.post('/add', response_model=schemas.Lenguaje)
+async def add_language(request:Request, db:Session=Depends(get_db)):
+    form_data = await request.form()
+    language_form = form_data['lenguaje']
+    languages = models.Lenguaje(nombre = language_form, id = 0)
+    db.add(languages)
+    db.commit()
+    return RedirectResponse('/', 303)
