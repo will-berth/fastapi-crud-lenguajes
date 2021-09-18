10dc61069db5ea845724e3b0758ef5632b14737a
Wilberth <wilberthlp4@gmail.com>
2021-09-18 14:09:06 -0500

    Create language model

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from sqlalchemy import Column, Integer, String
+from .Conexion import Base
+
+class Lenguaje(Base):
+    __tablename__ = 'lenguaje'
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(20))
