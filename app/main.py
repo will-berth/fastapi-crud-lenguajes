47d4aa7a4fc18ec9b308a36328e5402eb6301a68
Wilberth <wilberthlp4@gmail.com>
2021-09-19 13:37:29 -0500

    Add edit language function

--- app/main.py
+++ app/main.py
@@ -46,3 +46,23 @@ def delete_languages(request:Request, lenguaje_id:int, db:Session=Depends(get_db
     db.commit()
     respuesta = schemas.Respuesta(mensaje='Lenguaje eliminado exitosamente')
     return RedirectResponse('/', 303)
+
+@app.get('/editar/{lenguaje_id}', response_model=schemas.Lenguaje)
+def update_language(request:Request, lenguaje_id:int, db:Session=Depends(get_db)):
+    edit_language = db.query(models.Lenguaje).filter_by(id=lenguaje_id).first()
+    result_edit = jsonable_encoder(edit_language)
+    languages = db.query(models.Lenguaje).all()
+    result = jsonable_encoder(languages)
+    return templates.TemplateResponse('index.html', {'request': request, 'lista': result, 'editar': True, 'item_edit': result_edit})
+
+@app.post('/editar', response_model=schemas.Lenguaje)
+async def update(request:Request, db:Session=Depends(get_db)):
+    form_data = await request.form()
+    id_edit = int(form_data['lenguaje-id'])
+    lenguaje_name = form_data['lenguaje-edit']
+    lenguaje = db.query(models.Lenguaje).filter_by(id=id_edit).first()
+    lenguaje.nombre = lenguaje_name
+    db.commit()
+    db.refresh(lenguaje)
+    print(lenguaje_name)
+    return RedirectResponse('/', 303)
